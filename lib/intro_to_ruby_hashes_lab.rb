37072b2f0105cea37ccc46a76b247321ca30ed68
zara ahmadi <zatokey@gmail.com>
2020-05-11 01:19:06 +0000

    Done.

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,16 +1,16 @@
 def new_hash
-  # return an empty hash
+  arr = {};
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  arr = {:hello => 'world'};
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  arr = { name: 'Grace Hopper'};
 end
 
-
 def id_hash_generator(number)
-  # return a hash with a key :id assigned to the provided number
+  { id: number }
 end
