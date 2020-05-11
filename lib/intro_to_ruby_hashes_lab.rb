a3c345d0d8abd8209fc07f3fadae35fc889d7d66
zara ahmadi <zatokey@gmail.com>
2020-05-11 01:13:57 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,9 +1,9 @@
 def new_hash
-  # return an empty hash
+  arr = {};
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  arr = {:hello => 'world'};
 end
 
 def pioneer
