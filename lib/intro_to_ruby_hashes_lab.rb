92303186d649e834b4933982728ffe4ff90bae57
zara ahmadi <zatokey@gmail.com>
2020-05-11 01:18:51 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -11,9 +11,6 @@ def pioneer
   arr = { name: 'Grace Hopper'};
 end
 
-
 def id_hash_generator(number)
-  # return a hash with a key :id assigned to the provided number
-  {id : number}
-
+  { id: number }
 end
